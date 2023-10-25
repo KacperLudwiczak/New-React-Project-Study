b3f20caa5154e4d74ef70c9d4589c0bd4f2307ca
KacperLudwiczakeCERTO <kacper.ludwiczak@ecerto.io>
2023-10-25 23:50:02 +0200

    Data base on State

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import { useState } from "react";
 import Header from "./components/Header/Header";
 import CoreConcept from "./components/CoreConcepts";
 import { CORE_CONCEPTS } from "./data";
+import { EXAMPLES } from "./data";
 import TabButton from "./components/TabButton";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please Click");
+  const [selectedTopic, setSelectedTopic] = useState("components");
 
   function handleSelect(selectedButton) {
     // console.log(selectedButton);
@@ -35,14 +36,20 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("componets")}>
-              Componets
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
             </TabButton>
             <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
